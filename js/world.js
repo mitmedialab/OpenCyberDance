6ed9dc6eb88e25ac7d9edfd33ebf4bf37a0dba51
Phoomparin Mano <phoomparin@gmail.com>
2023-08-30 14:32:54 +0700

    chore: default model on load

--- js/world.js
+++ js/world.js
@@ -170,14 +170,14 @@ export class World {
   async setupCharacters() {
     this.addCharacter({
       name: 'first',
-      model: 'abstract',
+      model: 'kukpat',
       position: [-0.8, 0, 0],
       analyze: true,
     })
 
     this.addCharacter({
       name: 'second',
-      model: 'abstract',
+      model: 'kukpat',
       position: [0.8, 0, 0],
       freezeParams: true,
     })

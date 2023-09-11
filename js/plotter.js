883fd3c8e2b7fb6a55de68f301b9a7709816c97f
Phoomparin Mano <phoomparin@gmail.com>
2023-09-12 02:14:55 +0700

    fix: perf

--- js/plotter.js
+++ js/plotter.js
@@ -85,46 +85,6 @@ export class Plotter {
     s.gap = `${layout.py}px ${layout.px}px`
   }
 
-  createAnimation() {
-    const totalDuration = 10000
-    const delayBetweenPoints = totalDuration / this.windowSize
-
-    const previousY = (ctx) => {
-      return ctx.index === 0
-        ? ctx.chart.scales.y.getPixelForValue(100)
-        : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1]?.y
-    }
-
-    return {
-      x: {
-        type: 'number',
-        easing: 'linear',
-        duration: delayBetweenPoints,
-        from: NaN, // the point is initially skipped
-        delay(ctx) {
-          if (ctx.type !== 'data' || ctx.xStarted) {
-            return 0
-          }
-          ctx.xStarted = true
-          return ctx.index * delayBetweenPoints
-        },
-      },
-      y: {
-        type: 'number',
-        easing: 'linear',
-        duration: delayBetweenPoints,
-        from: previousY,
-        delay(ctx) {
-          if (ctx.type !== 'data' || ctx.yStarted) {
-            return 0
-          }
-          ctx.yStarted = true
-          return ctx.index * delayBetweenPoints
-        },
-      },
-    }
-  }
-
   createChart(chrId, trackId) {
     const track = this.world?.characterByName(chrId)?.trackByKey(trackId)
 

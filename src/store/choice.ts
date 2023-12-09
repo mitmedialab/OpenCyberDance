9797f99b6c4146f2496864ce8feaf5fb74aa19bc
Phoomparin Mano <phoomparin@gmail.com>
2023-12-10 00:15:10 +0700

    chore: regex error

--- src/store/choice.ts
+++ src/store/choice.ts
@@ -146,7 +146,7 @@ export function handleVoiceSelection(
       return selectChoice('curve')
     }
 
-    if (/(relation|shifting/i.test(input as string)) {
+    if (/(relation|shifting)/i.test(input as string)) {
       return selectChoice('shifting')
     }
 

3b66ebfff8aeb3a0728e5264d62208e5e688d717
Phoomparin Mano <phoomparin@gmail.com>
2025-02-22 12:04:39 +0700

    additional corrections

--- src/store/choice.ts
+++ src/store/choice.ts
@@ -181,7 +181,7 @@ export function handleVoiceSelection(input: string | number): boolean {
       return selectChoice('curve')
     }
 
-    if (/(relation|shifting|shooting)/i.test(input as string)) {
+    if (/(relation|shifting|shooting|shift)/i.test(input as string)) {
       return selectChoice('shifting')
     }
 
